cda9c26bb186ee2116dc3f61e0ed935d37037434
sanjeetsk <96577772+sanjeetsk@users.noreply.github.com>
2023-05-31 15:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS code here.
+function handleScroll() {
+      const slideInElements = document.querySelectorAll('.slide-in');
+    
+      slideInElements.forEach(element => {
+        // Calculate the position to trigger the slide-in effect
+        const slideInAt = (window.scrollY + window.innerHeight) - (element.offsetHeight / 2);
+        const imageBottom = element.offsetTop + element.offsetHeight;
+        const isHalfShown = slideInAt > element.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+    
+        // Add or remove the 'active' class based on scroll position
+        if (isHalfShown && isNotScrolledPast) {
+          element.classList.add('active');
+        } else {
+          element.classList.remove('active');
+        }
+      });
+    }
+
+window.addEventListener('scroll', debounce(handleScroll));
+
+
+
+
